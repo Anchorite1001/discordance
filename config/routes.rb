8bdc20b4de5b7a7341e37a73ef222f409e896d68
Yang Wu <yangwu.1415@gmail.com>
2021-01-25 12:21:14 +1100

    some initial mvc and heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'session/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'users#welcome'
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
+
+  resources :users
+  resources :episodes
+  resources :speakers
+
 end
