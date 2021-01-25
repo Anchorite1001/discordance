4b0d9632df141add4e31a83dace49b35b16befcb
Yang Wu <yangwu.1415@gmail.com>
2021-01-26 01:30:45 +1100

    user authorisation and crud for episodes

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,2 +1,42 @@
 class EpisodesController < ApplicationController
+  def index
+    @episodes = Episode.all
+  end
+
+  def new
+    check_for_admin
+    @episode = Episode.new
+  end
+
+  def create
+    episode = Episode.create episode_params
+    redirect_to episode_path(episode)
+  end
+
+  def show
+    @episode = Episode.find params[:id]
+  end
+
+  def edit
+    check_for_admin
+    @episode = Episode.find params[:id]
+  end
+
+  def update
+    episode = Episode.find params[:id]
+    episode.update episode_params
+    redirect_to episode_path(episode)
+  end
+
+  def destroy
+    check_for_admin
+    episode = Episode.find params[:id]
+    episode.destroy
+    redirect_to episodes_path
+  end
+
+  private
+  def episode_params
+    params.require(:episode).permit(:title, :created_at, :image, :player, :introduction)
+  end
 end
--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -1,2 +1,23 @@
 class SpeakersController < ApplicationController
+  def index
+    @speakers = Speaker.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
