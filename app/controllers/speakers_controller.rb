b657260c078330a0ce475c38afd8cadab10cedad
Yang Wu <yangwu.1415@gmail.com>
2021-01-26 18:46:10 +1100

    speaker mvc and bootstrap insert

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -4,20 +4,39 @@ class SpeakersController < ApplicationController
   end
 
   def new
+    check_for_admin
+    @speaker = Speaker.new
   end
 
   def create
+    speaker = Speaker.create speaker_params
+    redirect_to speaker_path(speaker)
   end
 
   def show
+    @speaker = Speaker.find params[:id]
   end
 
   def edit
+    check_for_admin
+    @speaker = Speaker.find params[:id]
   end
 
   def update
+    speaker = Speaker.find params[:id]
+    speaker.update speaker_params
+    redirect_to speaker_path(speaker)
   end
 
   def destroy
+    check_for_admin
+    speaker = Speaker.find params[:id]
+    speaker.destroy
+    redirect_to speakers_path
+  end
+
+  private
+  def speaker_params
+    params.require(:speaker).permit(:name, :host_or_guest, :image, :introduction)
   end
 end
