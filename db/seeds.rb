2da31f745cd38795dae7e2e6667030d1dc65f4a3
Yang Wu <yangwu.1415@gmail.com>
2021-01-28 14:41:22 +1100

    change seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,21 @@
 User.destroy_all
 u1 = User.create :email => 'yangwu@discordance.co', :password => 'chicken', :admin => true
 u2 = User.create :email => 'liangyu@discordance.co', :password => 'chicken', :admin => true
+u3 = User.create :email => 'testing1@discordance.co', :password => 'chicken', :admin => false
 puts "#{User.count} users"
 
 Episode.destroy_all
-e1 = Episode.create :title => 'S1E1'
-e2 = Episode.create :title => 'S1E2'
+e1 = Episode.create :title => "S1E1 | MeToo新视角：地市级晚报，媒体商业化与微博“中产”女权？", :image => 'ep1.JPG'
+e2 = Episode.create :title => 'S1E2 | 把约会作为研究对象：亚裔在澳洲的情感生活',
+:image => 'ep2.JPG'
 puts "#{Episode.count} episodes"
 
 Speaker.destroy_all
-s1 = Speaker.create :name => 'Liangyu Sun', :host_or_guest => 'host'
-s2 = Speaker.create :name => 'Yang Wu', :host_or_guest => 'host'
-s3 = Speaker.create :name => 'Shan Huang', :host_or_guest => 'guest'
-s4 = Speaker.create :name => 'Tingting Liu', :host_or_guest => 'guest'
-s5 = Speaker.create :name => 'Xibei Wang', :host_or_guest => 'guest'
+s1 = Speaker.create :name => 'Liangyu Sun', :host_or_guest => 'Host', :image => 'LiangyuSun.png'
+s2 = Speaker.create :name => 'Yang Wu', :host_or_guest => 'Host', :image => 'YangWu.png'
+s3 = Speaker.create :name => 'Shan Huang', :host_or_guest => 'Guest', :image => 'ShanHuang.png'
+s4 = Speaker.create :name => 'Tingting Liu', :host_or_guest => 'Guest', :image => 'TingtingLiu.png'
+s5 = Speaker.create :name => 'Xibei Wang', :host_or_guest => 'Guest', :image => 'XibeiWang.png'
 puts "#{Speaker.count} speakers"
 
 puts "users and episodes"
